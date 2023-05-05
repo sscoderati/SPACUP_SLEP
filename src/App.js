84cf6ed9298c7ff922992bb4edaf1514fc34d7a1
Cheshier <chungup5495@gmail.com>
2023-05-05 09:27:10 +0900

    delete bootstrap and static pages

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { Component } from "./core/util";
+
+export default class App extends Component {
+    render() {
+        const routerView = document.createElement('router-view')
+
+        this.el.append(routerView)
+    }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+import { createRouter } from "../core/util";
+import Home from "./Home";
+
+export default createRouter([
+    { path: '/#', component: Home}
+])
