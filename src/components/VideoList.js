7526d2c859de91da8ab9e4157cbe305b4ef1fbfd
Cheshier <chungup5495@gmail.com>
2023-05-08 07:35:12 +0900

    add loading animation & styled + error fix

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,5 +1,6 @@
 import { Component } from "../core/util";
 import videoStore from '../store/video'
+import VideoItem from "./VideoItem";
 
 export default class VideoList extends Component {
     constructor() {
@@ -7,18 +8,27 @@ export default class VideoList extends Component {
         videoStore.subscribe('videos', () => {
             this.render()
         })
+        videoStore.subscribe('loading', () => {
+            this.render()
+        })
     }
     render() {
         this.el.classList.add('video-list')
         this.el.innerHTML = /* html */ `
             <div class="videos"></div>
+            <div class="the-loader hide"></div>
         `
 
         const videosEl = this.el.querySelector('.videos')
         videosEl.append(
-            videoStore.state.videos.map(video => {
-                return video.title
-            })
+            ...videoStore.state.videos.map(video => new VideoItem({
+                video
+            }).el)
         )
+
+        const loaderEl = this.el.querySelector('.the-loader')
+        videoStore.state.loading
+        ? loaderEl.classList.remove('hide')
+        : loaderEl.classList.add('hide')
     }
 }
--- src/store/video.js
+++ src/store/video.js
@@ -4,17 +4,19 @@ import axios from "axios";
 const store = new Store({
     searchText: '',
     page: 1,
-    videos: []
+    videos: [],
+    loading: false
 })
 
 export default store
-export const searchVideos = async (page, searchText) => {
+export const searchVideos = async page => {
+    store.state.loading = true
+    store.state.page = page
     if (page === 1) {
-        store.state.page = 1
         store.state.videos = []
     }
     // const res = await fetch(`http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701?serviceKey=891a9d78-cd84-406e-a182-7472e7b6919f&pageNo=${page}`)
-    const res = await axios.get(`http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701?serviceKey=891a9d78-cd84-406e-a182-7472e7b6919f&numOfRows=100&pageNo=${page}`)
+    const res = await axios.get(`http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701?serviceKey=891a9d78-cd84-406e-a182-7472e7b6919f&numOfRows=12580&pageNo=${page}`)
     const Data = res.data.response.body.items.item
 
     let searched = []
@@ -24,7 +26,7 @@ export const searchVideos = async (page, searchText) => {
         }
     })
     store.state.videos = [
-        ...store.state.videos,
         ...searched
     ]
+    store.state.loading = false
 }
