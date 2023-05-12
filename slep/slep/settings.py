5b8296a917749ab649e26f95482d4b431987ac88
Hyoungsooo <aaa57403@gmail.com>
2023-05-12 11:15:45 +0900

    cors

--- slep/slep/settings.py
+++ slep/slep/settings.py
@@ -179,6 +179,5 @@ CACHES = {
 
 API_KEY = '891a9d78-cd84-406e-a182-7472e7b6919f'
 
-CORS_ORIGIN_WHITELIST = ['http://0.0.0.0:5085',
-                        'http://localhost:5085', 'http://127.0.0.1:5085','specup.kro.kr', 'http://www.specup.kro.kr', 'specup']
-CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
