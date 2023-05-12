d109b1a17b1a10c9eedba13908a3c0ab3fc372c5
Cheshier <chungup5495@gmail.com>
2023-05-12 09:57:12 +0900

    added view video detail & minimum styled

--- src/routes/Video.js
+++ src/routes/Video.js
@@ -4,6 +4,35 @@ import videoStore, { getVideoDetails } from '../store/video'
 export default class Video extends Component {
     render() {
         getVideoDetails(decodeURI(history.state.title))
-        console.log(videoStore.state.video)
+        const Video = videoStore.state.video
+        const linkSet = Video['subDescription']
+        const linkArr = linkSet.split('||')
+        const vidSrc = linkArr[0].slice(linkArr[0].indexOf(':') + 1)
+        let imgArr = linkArr[1].split(',')
+        imgArr[0] = imgArr[0].slice(imgArr[0].indexOf(':') + 1)
+        console.log(imgArr)
+
+        this.el.classList.add('container', 'the-video')
+        this.el.innerHTML = /* html */ `
+            <div class="video">
+                <h3>수어 영상</h3>
+                <video src="${vidSrc}" controls></video>
+            </div>
+            <div class="labels">
+                <h3>수어 정보</h3>
+                <p><span>${Video['alternativeTitle']}</span></p>
+                <h3>수어 설명</h3>
+                <p><span>${Video['description']}</span></p>
+            </div>
+            <div class="images">
+                <h3>그림 설명</h3>
+                <div class="img-layout">
+                    ${imgArr.map(imgEl => {
+                        // return imgEl !== ',' ? `<img src=${imgEl} />` : ''
+                        return `<img src="${imgEl}" />`
+                    })}
+                </div>
+            </div>
+            `
     }
 }
