fe9a2fb0fbf5ea52570a78f6fb11d99cd102e78e
Cheshier <chungup5495@gmail.com>
2023-05-13 22:46:58 +0900

    add 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import { createRouter } from "../core/util";
 import Home from "./Home";
 import Video from "./Video";
 import About from "./About";
+import NotFound from "./NotFound"
 
 export default createRouter([
     { path: '#/', component: Home},
     { path: '#/video', component: Video },
-    { path: '#/about', component: About }
+    { path: '#/about', component: About },
+    { path: '.*', component: NotFound }
 ])
