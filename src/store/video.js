7204ecc96647b0ffa2b85a1317d1fdcac9430ad8
Cheshier <chungup5495@gmail.com>
2023-05-13 23:55:12 +0900

    add redirection on api request

--- src/store/video.js
+++ src/store/video.js
@@ -18,9 +18,12 @@ export const searchVideos = async page => {
         store.state.videos = []
         store.state.message = ''
     }
+    const host = window.location.hostname === "localhost"
+    ? `http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701?serviceKey=891a9d78-cd84-406e-a182-7472e7b6919f&numOfRows=12530&pageNo=${page}`
+    : "api"
     // const res = await fetch(`http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701?serviceKey=891a9d78-cd84-406e-a182-7472e7b6919f&pageNo=${page}`)
     try {
-        const res = await axios.get(`http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701?serviceKey=891a9d78-cd84-406e-a182-7472e7b6919f&numOfRows=12530&pageNo=${page}`)
+        const res = await axios.get(host)
         // const res = await axios.get(`http://www.specup.kro.kr/api/lang?title=${store.state.searchText}`)
         const Data = res.data.response.body.items.item
         const Response = res.statusText
