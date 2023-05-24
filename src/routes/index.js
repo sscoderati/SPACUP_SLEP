492eb424472644691937aa016fb328c7ab42e9eb
Cheshier <chungup5495@gmail.com>
2023-05-24 23:07:38 +0900

    add Learn page at header

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ import Home from "./Home";
 import Video from "./Video";
 import About from "./About";
 import NotFound from "./NotFound"
+import Learn from "./Learn"
 
 export default createRouter([
     { path: '#/', component: Home},
     { path: '#/video', component: Video },
+    { path: '#/learn', component: Learn },
     { path: '#/about', component: About },
     { path: '.*', component: NotFound }
 ])
